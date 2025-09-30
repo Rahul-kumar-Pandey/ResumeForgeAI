066c98e30d286a7009cd8ef4d7fa7dab38fdf56f
Rahul-kumar-Pandey <developer.rahulpandey@gmail.com>
2025-09-30 15:38:02 +0530

    First Commit

---
+++ ResumeForgeAI/src/main/java/com/ResumeForge/ResumeForgeAI/ResumeForgeAiApplication.java
@@ -0,0 +1,14 @@
+package com.ResumeForge.ResumeForgeAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeForgeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeForgeAiApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ ResumeForgeAI/src/test/java/com/ResumeForge/ResumeForgeAI/ResumeForgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ResumeForge.ResumeForgeAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeForgeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
